d55b0162e8403a4294a7426225da8a7ed918f4d4
monojkumar2001 <monojkumar57859893@gmail.com>
2025-10-31 21:26:02 +0600

    update

--- app/components/Home/Leadership1.jsx
+++ app/components/Home/Leadership1.jsx
@@ -9,7 +9,7 @@ const Leadership = () => {
       <div className="container">
         <div className="leadership-title">
           <h4>A Legacy of Leadership</h4>
-          <h1 className="section-title"> A Future of Hope</h1>
+          <h1 className="section-title leadership-titles"> A Future of Hope</h1>
         </div>
         <div className="row align-items-center">
           <div className="col-lg-6">
